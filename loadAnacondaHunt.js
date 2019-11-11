d06c0e3bdd4ad6a8348753ae631bf560352edbbd
Simar Preet Singh <simarpreetsinghkalra@gmail.com>
2019-11-11 18:57:22 +0530

    fixed file location issue

--- loadAnacondaHunt.js
+++ loadAnacondaHunt.js
@@ -1,1 +1,1 @@
-document.getElementById('anaconda_hunt').innerHTML='<object type="text/html" data="index.html"></object>';
+document.getElementById('anaconda_hunt').innerHTML='<object type="text/html" data="https://simarpreetsinghkalra.github.com./AnacondasHunt/index.html"></object>';
