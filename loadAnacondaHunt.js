e7fa4cfa349e826d1688ecdf106f4da8fd72e245
Simar Preet Singh <simarpreetsinghkalra@gmail.com>
2019-11-11 18:41:59 +0530

    figed wrong file name

--- loadAnacondaHunt.js
+++ loadAnacondaHunt.js
@@ -1,1 +1,1 @@
-document.getEementById('anaconda_hunt').innerHTML='<object type="text/html" data="home.html"></object>';
+document.getEementById('anaconda_hunt').innerHTML='<object type="text/html" data="index.html"></object>';
